7189efd461ea73bcdaddef6e594064080361b5e3
aac190002 <66142794+aac190002@users.noreply.github.com>
2020-07-22 16:42:55 -0500

    Added orientation to blocks

--- src/blocks.py
+++ src/blocks.py
@@ -28,93 +28,173 @@ class BlockType(Enum):
     T_INTERSECTION = 6
     CROSS = 7
     CURVED = 8
+    
+    
+class Orientation(Enum):
+    """Orientation of blocks, in 90 degree CCW increments. Says where the base of the block is"""
+    NORTH = 0
+    WEST = 1
+    SOUTH = 2
+    EAST = 3
 
 
 class EmptyBlock(object):
-    """Represents an empty block"""
+    """Represents an empty block. Can have any orientation"""
 
-    def __init__(self):
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.EMPTY
+        self.orientation = orientation
 
     def __str__(self):
         return " "
 
 
 class StartBlock(object):
-    """Represents a start block"""
+    """Represents a start block. Can have any orientation
 
-    def __init__(self):
+    Defined with exit to the south
+
+    +-----+
+    |  S  |
+    +-+ +-+
+      | |
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.START
+        self.orientation = orientation
 
     def __str__(self):
         return "S"
 
 
 class EndBlock(object):
-    """Represents an end block"""
+    """Represents an end block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north
+
+      | |
+    +-+ +-+
+    |  E  |
+    +-----+
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.END
+        self.orientation = orientation
 
     def __str__(self):
         return "E"
 
 
 class DeadEndBlock(object):
-    """Represents a dead end block"""
+    """Represents a dead end block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north
+
+      | |
+    +-+ +-+
+    |  X  |
+    +-----+
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.DEAD_END
+        self.orientation = orientation
 
     def __str__(self):
         return "X"
 
 
 class StraightBlock(object):
-    """Represents a straight block"""
+    """Represents a straight block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north. Is symmetric
+
+      | |
+      | |
+      | |
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.STRAIGHT
+        self.orientation = orientation
 
     def __str__(self):
         return "|" # Doesn't show orientation
 
 
 class RampBlock(object):
-    """Represents a ramp block"""
+    """Represents a ramp block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north (is at a higher level at the south)
+
+      | |
+      |V|
+      | |
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.RAMP
+        self.orientation = orientation
 
     def __str__(self):
         return "R"  # Doesn't show orientation
 
 
 class TIntersectionBlock(object):
-    """Represents a T intersection block"""
+    """Represents a T intersection block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north. Can be entered from any side
+
+         | |
+    -----+ +-----
+
+    -------------
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.T_INTERSECTION
+        self.orientation = orientation
 
     def __str__(self):
         return u"T"  # Doesn't show orientation :(
 
 
 class CrossBlock(object):
-    """Represents a cross block"""
+    """Represents a cross block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north. Can be entered from any side
+
+         | |
+    -----+ +-----
+
+    -----+ +-----
+         | |
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.CROSS
+        self.orientation = orientation
 
     def __str__(self):
         return "+"
 
 
 class CurvedBlock(object):
-    """Represents a curved block"""
+    """Represents a curved block. Can have any orientation
 
-    def __init__(self):
+    Defined with entrance to the north. Can be entered from any side
+
+    | |
+    | +-----
+    |
+    +-------
+    """
+
+    def __init__(self, orientation=Orientation.NORTH):
         self.block_type = BlockType.CURVED
+        self.orientation = orientation
 
     def __str__(self):
         return "L"  # Doesn't show orientation
